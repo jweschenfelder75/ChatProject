ce2ff636698b57a8267102da7fff16d73a5ab2f7
Jana Weschenfelder <jana.weschenfelder@gmx.de>
2025-04-23 16:52:59 +0200

    Implementation of the basic communication server - client.

--- modules/chat_server.py
+++ modules/chat_server.py
@@ -12,3 +12,8 @@ class ChatServer:
         server_socket.bind((socket.gethostname(), self.port))
         server_socket.listen(5)
         print(f'Listening on {socket.gethostname()}:{self.port}')
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print(f'Established connection to {client_address[0]}:{client_address[1]}')
+            client_socket.send(bytes('Hello there', 'utf-8'))
