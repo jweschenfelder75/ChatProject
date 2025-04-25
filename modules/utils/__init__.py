77f98a570527d2553c808a05d76350711970794a
Jana Weschenfelder <jana.weschenfelder@gmx.de>
2025-04-25 14:07:27 +0200

    Working on seperating UI from client logic. __init__.py's corrected (it worked without as well, but this looks better now)

--- modules/utils/__init__.py
+++ modules/utils/__init__.py
@@ -1,5 +1,5 @@
 import sys
 
-sys.path.append(sys.path[0] + "\\modules")
+sys.path.append(sys.path[0] + "\\modules\\utils")
 # PyCharm fix: https://stackoverflow.com/questions/36827962/pep8-import-not-at-top-of-file-with-sys-path
 from modules.utils.utils import *   # noqa: E402
